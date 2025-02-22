a2df616549434bbece3c9019ee505f231ff6a84d
mateusTegue <teusteguetorres2001@gmail.com>
2025-02-21 22:32:41 -0500

    configuracion de las imagenes

--- porfolioweb/urls.py
+++ porfolioweb/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path, include
-from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
