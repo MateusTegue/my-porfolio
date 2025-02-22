2db63e83a8629f84dab52085428c6c7dc404f57a
mateusTegue <teusteguetorres2001@gmail.com>
2025-02-21 21:56:35 -0500

    configuracion las imagenes

--- formacion/serializers.py
+++ formacion/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Formacion
 
 class FormacionSerializer(serializers.ModelSerializer):
+    imagen = serializers.SerializerMethodField()  # Modificamos el campo imagen
+
+    def get_imagen(self, obj):
+        request = self.context.get('request')  # Obtenemos el request para construir la URL
+        if obj.imagen:
+            return request.build_absolute_uri(obj.imagen.url)  # Devuelve URL completa
+        return None  # Si no hay imagen, retorna None
+
     class Meta:
         model = Formacion
         fields = ('id', 'title', 'description', 'imagen', 'date', 'url')
